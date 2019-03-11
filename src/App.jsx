91703c169df2db14367ef1f087185054c63bb825
Matt Cook <mcc1985@gmail.com>
2019-03-11 23:41:32 +0000

    Insert initial React Components & Styling
    React component for ChatBar and Message were created. These components where imported to App.jsx where they were rendered into a single container (then exported to index.jsx where they are assigned to a DOM root).
    
    Intial styles were inserted into home.scss.
    
    index.html was modified to take in React components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import ChatBar from './ChatBar.jsx';
+import Message from './Message.jsx';
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div className='container'>
+        <Message />
+        <ChatBar />
+      </div>
     );
   }
 }
-export default App;
