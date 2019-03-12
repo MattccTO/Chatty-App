00508d78deebaf572eb2d16781560e44868077ca
Matt Cook <mcc1985@gmail.com>
2019-03-12 21:16:15 +0000

    User input message
    The root app now sends a function as a prop to ChatBar. In ChatBar, an event is triggered when the user presses Enter in the message bar, which takes that message and calls the passed function with that data. The called function in the root app then updates the state which rerenders the page with the new message.

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,40 @@ export default class App extends Component {
     };
   }
 
+  addNewMessage = (message) => {
+      const newMessage = {
+        id: (this.state.message.length + 1),
+        type: 'incomingMessage',
+        username: this.state.currentUser,
+        content: message
+      };
+      const messages = this.state.message.concat(newMessage);
+      this.setState({message: messages});
+  };
+
   componentDidMount() {
-    console.log('componentDidMount');
-      setTimeout(() => {
-        console.log('Simulating incoming message');
-        // Add a new message to the list of messages in the data store
-        const newMessage = {
-          id: 8,
-          type: 'incomingMessage',
-          username: 'Michelle',
-          content: 'Hello there!'
-        };
-        const messages = this.state.message.concat(newMessage)
-        // Update the state of the app component.
-        // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({message: messages})
-    }, 3000);
+    // // console.log('componentDidMount');
+    // setTimeout(() => {
+    //   console.log('Simulating incoming message');
+    //   // Add a new message to the list of messages in the data store
+    //   const newMessage = {
+    //     id: 8,
+    //     type: 'incomingMessage',
+    //     username: 'Michelle',
+    //     content: 'Hello there!'
+    //   };
+    //   const messages = this.state.message.concat(newMessage)
+    //   // Update the state of the app component.
+    //   // Calling setState will trigger a call to render() in App and all child components.
+    //   this.setState({message: messages})
+    // }, 3000);
   }
 
   render() {
     return (
       <div className='container'>
         <MessageList message={this.state.message} />
-        <ChatBar currentUser={this.state.currentUser} />
+        <ChatBar currentUser={this.state.currentUser} addMsg={this.addNewMessage} />
       </div>
     );
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
 export default class ChatBar extends Component {
+  submitMsg = (event) => {
+    // this.props.addMsg()
+    if (event.keyCode == 13) {
+      const message = event.target.value;
+      this.props.addMsg(message);
+      event.target.value = '';
+    }
+  }
+
   render() {
     return (
       <footer className="chatbar">
         <input className="chatbar-username" defaultValue={this.props.currentUser} placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-message" onKeyDown={this.submitMsg} placeholder="Type a message and hit ENTER" />
       </footer>
     );
   }
