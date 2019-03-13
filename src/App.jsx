0d309067303910a8db35648a3bbaeef3ffd61c0e
Matt Cook <mcc1985@gmail.com>
2019-03-13 18:50:42 +0000

    Add Web Socket Server and Message Broadcasting Functionality
    Implemented a web socket server.
    
    Submitted messages are now sent to the wss, where a unique id is applied to the message object before it is broadcast to all clients and rendered into their apps.

--- src/App.jsx
+++ src/App.jsx
@@ -8,37 +8,31 @@ export default class App extends Component {
     super(props);
     this.state = {
       currentUser: 'Dev',
-      message: messagesStatic
+      message: []
     };
   }
 
   addNewMessage = (message) => {
-      const newMessage = {
-        id: (this.state.message.length + 1),
-        type: 'incomingMessage',
-        username: this.state.currentUser,
-        content: message
-      };
-      const messages = this.state.message.concat(newMessage);
-      this.setState({message: messages});
+    const newMessage = {
+      type: 'incomingMessage',
+      username: this.state.currentUser,
+      content: message
+    };
+    this.socket.send(JSON.stringify(newMessage));
   };
 
   componentDidMount() {
-    // // console.log('componentDidMount');
-    // setTimeout(() => {
-    //   console.log('Simulating incoming message');
-    //   // Add a new message to the list of messages in the data store
-    //   const newMessage = {
-    //     id: 8,
-    //     type: 'incomingMessage',
-    //     username: 'Michelle',
-    //     content: 'Hello there!'
-    //   };
-    //   const messages = this.state.message.concat(newMessage)
-    //   // Update the state of the app component.
-    //   // Calling setState will trigger a call to render() in App and all child components.
-    //   this.setState({message: messages})
-    // }, 3000);
+    this.socket = new WebSocket('ws:localhost:3001');
+
+    this.socket.addEventListener('open', (event) => {
+      console.log('Connected to server!');
+    });
+
+    this.socket.addEventListener('message', (event) => {
+      const newMessage = JSON.parse(event.data);
+      const oldMessage = this.state.message;
+      this.setState({ message: [...oldMessage, newMessage] });
+    });
   }
 
   render() {
